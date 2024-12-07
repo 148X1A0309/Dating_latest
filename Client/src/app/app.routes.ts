f42f5d0e917aee188c8e210d142224b9d654e7f2
Chandu <tandava0987@gmail.com>
2024-12-07 15:58:31 +0530

    feat/routing_configuration

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'access', pathMatch: 'full' },
+  { path: 'access',
+     loadChildren : () =>
+    import('./user-access/user-access.module').then((c)=>c.UserAccessModule)
+  },
+  { path: '**', redirectTo: 'access' }
+];
+@NgModule({
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
+  })
+  export class AppRoutingModule {}
+  
