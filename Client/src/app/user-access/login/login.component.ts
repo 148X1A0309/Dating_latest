74724eea4d92e05590a130b43f3fdcc8119ef126
Chandu <tandava0987@gmail.com>
2024-12-14 21:09:51 +0530

    login_Page

--- Client/src/app/user-access/login/login.component.ts
+++ Client/src/app/user-access/login/login.component.ts
@@ -1,11 +1,37 @@
+
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { UserAcess } from '../../interfaces/user-access';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  userPassword!: string;
+  userAcess: UserAcess = {
+    userName: '',
+    userPassword: '',
+  };
 
+  constructor(private router:Router){
+
+  }
+
+  login(userData: UserAcess) {
+    console.log(userData.userName);
+    this.router.navigate(['/login']);
+  }
+
+  register(){
+
+  }
+
+  cancel(){
+    
+  }
 }
